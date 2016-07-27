cf910fc0bcbec88cc436f0a05f775b7ae584b572
Léo Davesne <leo@davesne.net>
2016-07-27 21:04:37 +0200

    As an anonymous user, I want to share my emotion by sending an email: https://trello.com/c/T2GmZ6Zw/23-as-an-anonymous-user-i-want-to-share-my-emotion-by-sending-an-email

---
+++ app/src/main/java/com/example/leo_000/myapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.leo_000.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton imageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        imageButton = (ImageButton) findViewById(R.id.happy);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                /*Intent browserIntent =
+                        new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.mkyong.com"));
+                startActivity(browserIntent);*/
+
+                composeEmail();
+            }
+        });
+    }
+
+    public void composeEmail() {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"leo@davesne.net"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+        i.putExtra(Intent.EXTRA_TEXT   , "body of email");
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
